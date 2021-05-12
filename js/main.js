f02fc2c8e853877f88f91fcd1c47e0b85309eb8e
sebT-max <sebternest@hotmail.com>
2021-05-12 10:59:03 +0200

    header-fixed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+
 $(document).ready(function () {
 
 
@@ -25,40 +26,5 @@ $(document).ready(function () {
         gallery: { enabled: true }
     });
 
-
-    // Owl-carousel
-
-    $('.site-main .about-area .owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: true,
-        dots: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            560: {
-                items: 2
-            }
-        }
-    })
-
-    // sticky navigation menu
-
-    let nav_offset_top = $('.header_area').height() + 50;
-
-    function navbarFixed() {
-        if ($('.header_area').length) {
-            $(window).scroll(function () {
-                let scroll = $(window).scrollTop();
-                if (scroll >= nav_offset_top) {
-                    $('.header_area .main-menu').addClass('navbar_fixed');
-                } else {
-                    $('.header_area .main-menu').removeClass('navbar_fixed');
-                }
-            })
-        }
-    }
-
-    navbarFixed();
 
 });
